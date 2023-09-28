9d54a1d81c8a5d0350978e546fee200b6dac581a
BCStr <bvchhor@cpp.edu>
2023-09-27 23:51:41 -0700

    added Wait.cpp

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -3,12 +3,14 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <ProcessClient.h>
 #include "Wait.h"
 
 Wait::Wait(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
-    parser().setDescription("Wait for a process to change state");
+    parser().setDescription("Wait for a process to end");
     parser().registerPositional("PID", "The ID of the process to wait on");
 }
 
@@ -16,10 +18,9 @@ Wait::~Wait()
 {
 }
 
-Wait::Result Wait::exec()
-{
-    //TODO Implement command
+Wait::Result Wait::exec() {
+    ProcessID pid = (atoi(arguments().get("PID")));
+    waitpid(pid, 0, 0);
 
-    // Done
     return Success;
 }
