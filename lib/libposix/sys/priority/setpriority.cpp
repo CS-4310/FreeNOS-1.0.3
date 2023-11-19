1e9aad6dbee88294a6f3f6e114688f35fbaa58c8
Austin Aluzzi <aaaluzzi@cpp.edu>
2023-11-18 19:36:30 -0800

    Set priority by passing process info

--- lib/libposix/sys/priority/setpriority.cpp
+++ lib/libposix/sys/priority/setpriority.cpp
@@ -2,12 +2,23 @@
 #include <FreeNOS/User.h>
 #include "sys/priority.h"
 #include "sys/types.h"
+#include <ProcessClient.h>
 
 int setpriority(pid_t pid, int priority_level)
-{
-    const ulong result = (ulong) ProcessCtl(pid, SetPriorityLevel, (Address) priority_level);
-    if (result == 0) {
-        return priority_level;
+{   
+    const ProcessClient process;
+    ProcessClient::Info info;
+
+    const ProcessClient::Result infoResult = process.processInfo(pid, info);
+    if (infoResult != ProcessClient::Success) {
+        return -1;
     }
-    return result;
+
+    info.kernelState.priority_level = priority_level;
+
+    API::Result resultPriority = ProcessCtl(pid, SetPriorityLevel, (Address) &info.kernelState);
+    if (resultPriority != API::Success) {
+        return -1;
+    }
+    return priority_level;
 }
